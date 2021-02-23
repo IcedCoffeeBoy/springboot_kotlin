58251ad6e1d845da744bc7da80dcc4ee49d48316
Lee Ming Liang <dev.lminglia@gmail.com>
2021-02-23 09:57:16 +0800

    incremental refractoring of specification

--- src/main/kotlin/com/spring/fileupload/product/ProductSpecifications.kt
+++ src/main/kotlin/com/spring/fileupload/product/ProductSpecifications.kt
@@ -12,68 +12,68 @@ import javax.persistence.criteria.Root
  * List of specification for product
  */
 
-fun startInvoiceDate(date: LocalDate): Specification<Product?> {
-    return Specification { root: Root<Product?>, _: CriteriaQuery<*>?, cb: CriteriaBuilder ->
+fun startInvoiceDate(date: LocalDate): Specification<Product> {
+    return Specification { root: Root<Product>, _: CriteriaQuery<*>?, cb: CriteriaBuilder ->
         cb.greaterThanOrEqualTo(root.get(ProductInfo.INVOICE_DATE), LocalDateTime.of(date, LocalTime.MIN))
     }
 }
 
-fun endInvoiceDate(date: LocalDate): Specification<Product?> {
-    return Specification { root: Root<Product?>, _: CriteriaQuery<*>?, cb: CriteriaBuilder ->
+fun endInvoiceDate(date: LocalDate): Specification<Product> {
+    return Specification { root: Root<Product>, _: CriteriaQuery<*>?, cb: CriteriaBuilder ->
         cb.lessThanOrEqualTo(root.get(ProductInfo.INVOICE_DATE), LocalDateTime.of(date, LocalTime.MAX))
     }
 }
 
-fun fromPrice(price: Double): Specification<Product?> {
-    return Specification { root: Root<Product?>, _: CriteriaQuery<*>?, cb: CriteriaBuilder ->
+fun fromPrice(price: Double): Specification<Product> {
+    return Specification { root: Root<Product>, _: CriteriaQuery<*>?, cb: CriteriaBuilder ->
         cb.greaterThanOrEqualTo(root.get(ProductInfo.UNIT_PRICE), price)
     }
 }
 
-fun toPrice(price: Double): Specification<Product?> {
-    return Specification { root: Root<Product?>, _: CriteriaQuery<*>?, cb: CriteriaBuilder ->
+fun toPrice(price: Double): Specification<Product> {
+    return Specification { root: Root<Product>, _: CriteriaQuery<*>?, cb: CriteriaBuilder ->
         cb.lessThanOrEqualTo(root.get(ProductInfo.UNIT_PRICE), price)
     }
 }
 
-fun minQuantity(quantity: Long): Specification<Product?> {
-    return Specification { root: Root<Product?>, _: CriteriaQuery<*>?, cb: CriteriaBuilder ->
+fun minQuantity(quantity: Long): Specification<Product> {
+    return Specification { root: Root<Product>, _: CriteriaQuery<*>?, cb: CriteriaBuilder ->
         cb.greaterThanOrEqualTo(root.get(ProductInfo.QUANTITY), quantity)
     }
 }
 
-fun maxQuantity(quantity: Long): Specification<Product?> {
-    return Specification { root: Root<Product?>, _: CriteriaQuery<*>?, cb: CriteriaBuilder ->
+fun maxQuantity(quantity: Long): Specification<Product> {
+    return Specification { root: Root<Product>, _: CriteriaQuery<*>?, cb: CriteriaBuilder ->
         cb.lessThanOrEqualTo(root.get(ProductInfo.QUANTITY), quantity)
     }
 }
 
-fun byInvoiceNo(invoiceNo: String): Specification<Product?> {
-    return Specification { root: Root<Product?>, _: CriteriaQuery<*>?, cb: CriteriaBuilder ->
+fun byInvoiceNo(invoiceNo: String): Specification<Product> {
+    return Specification { root: Root<Product>, _: CriteriaQuery<*>?, cb: CriteriaBuilder ->
         cb.equal(root.get<Any>(ProductInfo.INVOICE_NO), invoiceNo)
     }
 }
 
-fun byStockCode(stockCode: String): Specification<Product?> {
-    return Specification { root: Root<Product?>, _: CriteriaQuery<*>?, cb: CriteriaBuilder ->
+fun byStockCode(stockCode: String): Specification<Product> {
+    return Specification { root: Root<Product>, _: CriteriaQuery<*>?, cb: CriteriaBuilder ->
         cb.equal(root.get<Any>(ProductInfo.STOCK_CODE), stockCode)
     }
 }
 
-fun byCustomerId(customerId: String): Specification<Product?> {
-    return Specification { root: Root<Product?>, _: CriteriaQuery<*>?, cb: CriteriaBuilder ->
+fun byCustomerId(customerId: String): Specification<Product> {
+    return Specification { root: Root<Product>, _: CriteriaQuery<*>?, cb: CriteriaBuilder ->
         cb.equal(root.get<Any>(ProductInfo.CUSTOMER_ID), customerId)
     }
 }
 
-fun byCountry(country: String): Specification<Product?> {
-    return Specification { root: Root<Product?>, _: CriteriaQuery<*>?, cb: CriteriaBuilder ->
+fun byCountry(country: String): Specification<Product> {
+    return Specification { root: Root<Product>, _: CriteriaQuery<*>?, cb: CriteriaBuilder ->
         cb.like(cb.lower(root.get(ProductInfo.COUNTRY)), "%" + country.toLowerCase() + "%")
     }
 }
 
-fun descriptionsContains(keywords: String): Specification<Product?> {
-    return Specification { root: Root<Product?>, _: CriteriaQuery<*>?, cb: CriteriaBuilder ->
+fun descriptionsContains(keywords: String): Specification<Product> {
+    return Specification { root: Root<Product>, _: CriteriaQuery<*>?, cb: CriteriaBuilder ->
         cb.like(cb.lower(root.get(ProductInfo.DESCRIPTION)), "%" + keywords.toLowerCase() + "%")
     }
 }
